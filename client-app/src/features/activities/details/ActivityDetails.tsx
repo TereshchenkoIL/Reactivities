66e47b7e5508975aaa11a3f946d5b85ec83c3cc2
TereshchenkoIL <teddipl73@gmail.com>
2021-08-27 09:40:33 +0300

    Client side attendance added

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
-import {useParams } from "react-router-dom";
-import {Grid } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
 import AcctivityDetailedSidebar from "./ActivityDetaildeSidebat";
@@ -10,29 +10,28 @@ import AcctivityDetailedHeader from "./ActivityDetailedHeader";
 import AcctivityDetailedInfo from "./ActivityDetailedInfo";
 
 
+export default observer(function ActivityDetails() {
 
-export default observer( function ActivityDetails(){
-
-    const {activityStore} = useStore();
-    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
-    const {id} = useParams<{id: string}>();
+    const { activityStore } = useStore();
+    const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
+    const { id } = useParams<{ id: string }>();
 
     useEffect(() => {
-        if(id) loadActivity(id)
+        if (id) loadActivity(id)
     }, [id, loadActivity])
 
     console.log(activity)
-    if(loadingInitial || !activity) return <LoadingComponent content='' />;
+    if (loadingInitial || !activity) return <LoadingComponent content='' />;
 
-    return(
+    return (
         <Grid>
             <Grid.Column width={10}>
-                <AcctivityDetailedHeader activity={activity}/>
+                <AcctivityDetailedHeader activity={activity} />
                 <AcctivityDetailedInfo activity={activity} />
                 <AcctivityDetailedChat />
             </Grid.Column>
             <Grid.Column width={6}>
-                <AcctivityDetailedSidebar />
+                <AcctivityDetailedSidebar activity={activity} />
             </Grid.Column>
         </Grid>
     );
