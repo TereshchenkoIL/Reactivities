2ce52f5619287a7a05ba76baff4ab39fb0fb619d
TereshchenkoIL <teddipl73@gmail.com>
2021-09-15 19:29:21 +0300

    Upload image bug fixed

--- API/Startup.cs
+++ API/Startup.cs
@@ -61,13 +61,13 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCsp(opt => opt
+            app.UseCspReportOnly(opt => opt
                 .BlockAllMixedContent()
                 .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:", "data:", "filesystem:"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-W5g3N5ISTvfjG3ac0QGCULzXlsFSy0+LvLYb1uTIZz4="))
             );
             
---
+++ README.md
@@ -0,0 +1,17 @@
+# Reactivities
+
+Reactivities is a social network that allows users to create activities for people to attend them.
+
+---
+
+# Functionality
+
+- Register
+- Login/Logout
+- Create/Edit activity
+- Attend/Unattend activity
+- View user profiles
+- Upload/Delete photos - stored in Cloudinary
+- Activity real time chat (SignalR)
+- Filter activities
+- Edit profile
